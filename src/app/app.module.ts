161ae831fbd4613df23ad33806a54d44be28b01e
RafaelMatheus <rafaelmatheusdecastro@hotmail.com.br>
2018-11-02 14:14:23 -0300

    Fazendo requisição get para categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule }from '@angular/common/http'
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
@@ -13,6 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { API_CONFIG } from "../../config/api.config";
+import { CategoriaDto } from "../../model/categoria.dto";
+import { Observable } from "rxjs/Rx";
+
+@Injectable()
+export class CategoriaService{
+    constructor(public http:HttpClient){
+        
+    }
+    findAll() : Observable<CategoriaDto[]> {
+         return this.http.get(`${API_CONFIG.baseUrl}/categorias`);
+
+    }
+}   
