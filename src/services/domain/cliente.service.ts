15f702052e72e16fca23d69788b37b4797499d5b
RafaelMatheus <rafaelmatheusdecastro@hotmail.com.br>
2018-11-03 11:36:32 -0300

    Adicionado interceptor para requisição de login com header

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -11,11 +11,8 @@ export class ClienteService{
 
  findByEmail(email: string) : Observable<ClienteDto>{
 
-    let token = this.storage.getLocalUser().token;
-    let authHeader = new HttpHeaders({'Authorization': 'Bearer '+token})
     return this.http.get<ClienteDto>(`
-                                    ${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-                                    {'headers':authHeader});
+                                    ${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
  }
 
  getImageFromBucket(id : string) : Observable<any>{
